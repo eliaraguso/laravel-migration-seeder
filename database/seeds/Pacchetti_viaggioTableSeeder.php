9266630c620e75fc642b077f2c11f7073ce8c6a9
eliaraguso <elia.raguso@gmail.com>
2021-11-09 18:09:30 +0100

    Creato seeder per la tabella appena creata("pacchetti_viaggio")

---
+++ database/seeds/Pacchetti_viaggioTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class Pacchetti_viaggioTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
